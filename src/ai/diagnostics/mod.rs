f08c35964baa3b8c5dbc67c91a6d3e3368f8f2ac
Simon A. Berger <simberger@gmail.com>
2022-04-03 14:19:47 +0200

    got action name from Actor. Now what...

--- src/ai/diagnostics/mod.rs
+++ src/ai/diagnostics/mod.rs
@@ -2,7 +2,7 @@ use bevy::prelude::*;
 use big_brain::prelude::*;
 
 #[derive(Default)]
-pub struct DiagnosticsTarget(Vec<Entity>);
+pub struct DiagnosticsTarget(Vec<(Entity, Entity)>);
 
 // mixed experiments for extracting diagnostic info from big_brain Actors (strictly for debug visualization etc.)
 
@@ -17,39 +17,41 @@ fn select_actions_system(mut targets: ResMut<DiagnosticsTarget>, query: Query<(E
     for (entity, Actor(actor_entity)) in query.iter() {
         // entity
         info!("actor {:?} for {:?}", entity, actor_entity);
-        targets.0.push(entity);
+        targets.0.push((entity, *actor_entity));
     }
 }
 
 fn show_actions_system(world: &mut World) {
     if let Some(DiagnosticsTarget(entities)) = world.get_resource::<DiagnosticsTarget>() {
-        for entity in entities.iter() {
+        for (entity, actor_entity) in entities.iter() {
             if let Some(entity_ref) = world.get_entity(*entity) {
-                let components = entity_ref
-                    .archetype()
-                    .table_components()
-                    .iter()
-                    .map(|component_id| world.components().get_info(*component_id).unwrap().name())
-                    .collect::<Vec<_>>();
+                for component_id in entity_ref.archetype().table_components().iter() {
+                    if let Some(info) = world.components().get_info(*component_id) {
+                        if info.name().starts_with("game1::ai::actions::") {
+                            if let Some((_, action_name)) = info.name().rsplit_once("::") {
+                                println!("action: {}", action_name);
+                                // TODO: how to continue from here? we want to set set action name as overlay text of the actor_entity...
 
-                info!("diagnostic entity: {:?}: {:?}", entity, components);
+                                // if let Some(actor_entity_ref) = world.get_entity(actor_entity) {
+                                //     // actor_entity_ref.
+                                //     // world.
+                                // }
+                            }
+                        }
+                    }
+                }
             }
         }
     }
-
-    // world.get_entity(entity).
-    // for (entity, Actor(actor_entity)) in query.iter() {
-    //     // entity
-    //     info!("actor {:?} for {:?}", entity, actor_entity);
-    // }
 }
 
 pub struct AiDiagnosticsPlugin;
 impl Plugin for AiDiagnosticsPlugin {
     fn build(&self, app: &mut App) {
         app.init_resource::<DiagnosticsTarget>()
-            .add_system(show_scores_system)
-            // .add_system(select_actions_system)
-            .add_system(show_actions_system.exclusive_system());
+        // .add_system(show_scores_system)
+        // .add_system(select_actions_system)
+        // .add_system(show_actions_system.exclusive_system())
+        ;
     }
 }
